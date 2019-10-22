34334d0cdfae260b013cb51f77222f853edb79ce
JM Sumague <jm.sumague@opsolutions.biz>
2019-10-22 14:18:40 +0800

    Initial Commit

---
+++ iospraktis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iospraktis
+//
+//  Created by OPSolutions on 22/10/2019.
+//  Copyright © 2019 OPSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iospraktis/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iospraktis
+//
+//  Created by OPSolutions on 22/10/2019.
+//  Copyright © 2019 OPSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
